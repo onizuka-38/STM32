2a16e2c5f335693eb331a176a2f28d808681fb8a
onizuka-38 <bdh9938@gmail.com>
2025-06-15 00:34:25 +0900

    stm32

---
+++ BMEAL2/BMEAL/SEVEN_SEGMENTS/SEVEN_SEGMENTS_cfg.h
@@ -0,0 +1,17 @@
+/*
+ * File: SEVEN_SEGMENTS_cfg.h
+ * Driver Name: [[ 7-Segments Display ]]
+ * SW Layer:   BMEAL
+ * Created on: Sep 22, 2023
+ * Author:     JeongWhan Lee
+ *
+ */
+
+#ifndef SEVEN_SEGMENTS_CFG_H_
+#define SEVEN_SEGMENTS_CFG_H_
+
+#include "SEVEN_SEGMENTS.h"
+
+extern const SEVEN_SEG_CfgType SEVEN_SEG_CfgParam[SEVEN_SEG_UNITS];
+
+#endif /* SEVEN_SEGMENTS_CFG_H_ */
